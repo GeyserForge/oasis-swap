1abfd907edab5396764cb27e765867598fc7d944
Swede <swede@trident.game>
2023-03-22 00:31:44 +0000

    Simplify router interface

--- contracts/oasis/OasisSwapERC20.sol
+++ contracts/oasis/OasisSwapERC20.sol
@@ -31,7 +31,7 @@ contract OasisSwapERC20 {
             abi.encode(
                 keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
                 keccak256(bytes(name)),
-                keccak256(bytes('1')), // oops
+                keccak256(bytes('1')),
                 chainId,
                 address(this)
             )
